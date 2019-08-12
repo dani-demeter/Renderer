b3f3f10ef2fa8c336920ac5cbba5c89921a267c0
Dani Demeter <flamegiraffes@gmail.com>
2019-08-12 13:13:49 +0200

    initial commit

---
+++ colorPalette.js
@@ -0,0 +1,25 @@
+var coolors = {};
+
+function setupCoolors(){
+   coolors.white = color(245, 240, 246);
+   coolors.black = color(28, 29, 32);
+   coolors.gray = color(175, 175, 175);
+   coolors.blue = color(42, 183, 202);
+   coolors.dblue = color(0, 72, 124);
+   coolors.ddblue = color(36,30,78);
+   coolors.rasp = color(158,0,49);
+   coolors.orange = color(250,169,22);
+   coolors.purp = color(102, 42, 175);
+   coolors.mar = color(51, 0, 54);
+   coolors.gold = color(255, 199, 0);
+}
+// --white:#F5F0F6;
+// --black:#1C1D20;
+// --gray:#DCDCDC;
+// --blue:#2AB7CA;
+// --dblue:#00487C;
+// --rasp:#9E0031;
+// --orange:#FAA916;
+// --purp:#662AAF;
+// --mar:#330036;
+// --gold: #FFC700;
---
+++ index.js
@@ -0,0 +1,203 @@
+var cnv;
+
+var sceneCamera;
+var spheres;
+var light;
+
+var img;
+
+var minDist = 0.01;
+
+function setup() {
+   // colorMode(HSL);
+   setupCoolors();
+   setupCamera();
+   setupSpheres();
+   setupLights();
+   cnv = createCanvas(window.innerWidth, window.innerHeight);
+   cnv.position(0, 0);
+   // colorMode(RGB);
+   // colorMode(HSL);
+   background(coolors.black);
+   img = createImage(sceneCamera.res.width, sceneCamera.res.height);
+   img.loadPixels();
+   render();
+   img.updatePixels();
+   image(img, (window.innerWidth - sceneCamera.res.width) / 2, (window.innerHeight - sceneCamera.res.height) / 2);
+}
+
+function setupCamera() {
+   sceneCamera = {
+      center: createVector(-2, 0.5, 0),
+      forward: createVector(1, 0, 0),
+      up: createVector(0, 1, 0),
+      res: {
+         width: window.innerWidth,
+         height: window.innerHeight
+      },
+      // res: {
+      //    width: 10,
+      //    height: 10
+      // },
+      fov: 60,
+      renderDist: 10
+   };
+   sceneCamera.right = p5.Vector.cross(sceneCamera.forward, sceneCamera.up);
+}
+
+function setupSpheres() {
+   spheres = [{
+      center: createVector(2, 0, 0),
+      r: 0.5,
+      color: coolors.rasp
+   }, {
+      center: createVector(3, 1, 0),
+      r: 0.5,
+      color: coolors.blue
+   }, {
+      center: createVector(1, 0.5, -0.5),
+      r: 0.25,
+      color: color(100, 80, 80)
+   }];
+}
+
+function setupLights(){
+   light = {
+      center: createVector(1, 5, 0)
+   };
+}
+
+function dist2Point(point1, point2) {
+   return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2) + Math.pow(point1.z - point2.z, 2));
+}
+
+function dist2Sphere(point, sphere) {
+   return dist2Point(point, sphere.center) - sphere.r;
+}
+
+function getNearestObject(point) {
+   var min = dist2Sphere(point, spheres[0]);
+   var objHit = spheres[0];
+   for (var i = 0; i < spheres.length; i++) {
+      var newDist = dist2Sphere(point, spheres[i]);
+      if (newDist < min) {
+         min = newDist;
+         objHit = spheres[i];
+      }
+   }
+   return {
+      dist: min,
+      objHit
+   };
+}
+
+function castRay(direction) {
+   var dist = 0;
+   var point = sceneCamera.center;
+   var hit = false;
+   var objHit = null;
+   var norm = null;
+   while (dist < sceneCamera.renderDist && !hit) {
+      var nextSphere = marchSphere(point);
+      if (nextSphere.result == 'hit') {
+         hit = true;
+         return {
+            hit,
+            dist,
+            objHit: nextSphere.objHit,
+            norm: nextSphere.norm,
+            PoH: point
+         };
+      } else {
+         dist += nextSphere.dist;
+         point = {
+            x: point.x + nextSphere.dist * direction.x,
+            y: point.y + nextSphere.dist * direction.y,
+            z: point.z + nextSphere.dist * direction.z
+         };
+      }
+   }
+   return {
+      hit,
+      dist,
+   };
+
+}
+
+function getNormalAt(point, obj) {
+   var e = 0.01;
+   var v = createVector(
+      dist2Sphere(createVector(point.x + e, point.y, point.z), obj) -
+      dist2Sphere(createVector(point.x - e, point.y, point.z), obj),
+      dist2Sphere(createVector(point.x, point.y + e, point.z), obj) -
+      dist2Sphere(createVector(point.x, point.y - e, point.z), obj),
+      dist2Sphere(createVector(point.x, point.y, point.z + e), obj) -
+      dist2Sphere(createVector(point.x, point.y, point.z - e), obj),
+   );
+
+   return v.normalize();
+}
+
+function marchSphere(point) {
+   var nearestObject = getNearestObject(point);
+   var dist = nearestObject.dist;
+   if (dist < minDist) {
+      var norm = getNormalAt(point, nearestObject.objHit);
+      return {
+         result: 'hit',
+         objHit: nearestObject.objHit,
+         norm,
+      }
+   } else {
+      return {
+         result: 'no hit',
+         dist: dist
+      }
+   }
+}
+
+function render() {
+   // y   x
+   // |  /
+   // | /
+   // |/________z
+   colorMode(HSL);
+   for (var i = -sceneCamera.res.width / 2; i < sceneCamera.res.width / 2; i++) {
+      for (var j = -sceneCamera.res.height / 2; j < sceneCamera.res.height / 2; j++) {
+         // sceneCamera.direction*sceneCamera.renderDist+i*sceneCamera.right+j*sceneCamera.up
+         var vec = rotateAround(sceneCamera.forward, "y", -Math.PI * 2 * i * sceneCamera.fov * 1.0 / (sceneCamera.res.width * 360));
+         vec = rotateAround(vec, "z", -Math.PI * 2 * j * sceneCamera.fov * 1.0 / (sceneCamera.res.width * 360));
+         var ray = castRay(vec);
+         if (ray.hit) {
+            var d = createVector(light.center.x-ray.PoH.x, light.center.y-ray.PoH.y, light.center.z-ray.PoH.z);
+            var cosSim = p5.Vector.dot(ray.norm, d.normalize());
+            cosSim = cosSim<0 ? 0 : cosSim;
+            var c = color(hue(ray.objHit.color), saturation(ray.objHit.color), cosSim*lightness(ray.objHit.color));
+            img.set(i + sceneCamera.res.width / 2, j + sceneCamera.res.height / 2, c);
+         } else {
+            img.set(i + sceneCamera.res.width / 2, j + sceneCamera.res.height / 2, coolors.black);
+         }
+      }
+   }
+}
+
+function rotateAround(v, axis, angle) {
+   // console.log(axis + " " + angle);
+   var resx = 0;
+   var resy = 0;
+   var resz = 0;
+   if (axis == "x") {
+      resx = v.x;
+      resy = Math.cos(angle) * v.y - Math.sin(angle) * v.z;
+      resz = Math.sin(angle) * v.y + Math.cos(angle) * v.z;
+   } else if (axis == "y") {
+      resx = Math.cos(angle) * v.x + Math.sin(angle) * v.z;
+      resy = v.y;
+      resz = -Math.sin(angle) * v.x + Math.cos(angle) * v.z;
+   } else if (axis == "z") {
+      resx = Math.cos(angle) * v.x - Math.sin(angle) * v.y;
+      resy = Math.sin(angle) * v.x + Math.cos(angle) * v.y;
+      resz = v.z;
+   }
+   return createVector(resx, resy, resz);
+}
