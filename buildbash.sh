3b58ddd40fadcc7030e2407cd7d506a6fa23b1a0
Dani Demeter <flamegiraffes@gmail.com>
2019-11-27 19:48:14 +0100

    removed multiple commands in if

--- buildbash.sh
+++ buildbash.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if not exist SME19_fork\ (
-echo 'getting a fork!'
-git clone https://github.com/flamegiraffe/SME19_fork.git
-) else (
-echo 'already have a fork'
-)
+if not exist SME19_fork\ git clone https://github.com/flamegiraffe/SME19_fork.git
