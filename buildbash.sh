9b224ded6e9e31151a735375d71d85a08851d54b
Dani Demeter <flamegiraffes@gmail.com>
2019-11-27 19:54:30 +0100

    made td repo public

--- buildbash.sh
+++ buildbash.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$SME19_fork" ]; then
   	git clone https://github.com/flamegiraffe/SME19_fork.git
-	echo 'getting a fork'
+	echo 'getting a fork!'
    else
 	echo 'already got a fork'
 fi
