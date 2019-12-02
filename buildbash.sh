d3ce44355a1193678b6c861939a9f9ac29bc6080
Dani Demeter <flamegiraffes@gmail.com>
2019-12-02 12:46:39 +0100

    removed sme files

--- buildbash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "$SME19_fork" ]; then
-  	git clone https://github.com/flamegiraffe/SME19_fork.git
-	echo 'getting a fork!'
-   else
-	echo 'already got a fork'
-fi
--- testbash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "starting test from bash script"
-echo $JAVA_HOME
-java -jar SME19_fork/workspace_implementation/FromEclipse.jar
